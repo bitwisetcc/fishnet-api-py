38e0652c360121cb65a8da5df06d8175b7e5b748
JorgeTerence <jorge.terence@outlook.com>
2024-10-23 20:43:06 +0000

    started sales filter

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,4 @@
+from bson import Regex
 from flask import Blueprint, jsonify, request
 
 from connections import db
@@ -6,49 +7,55 @@ sales = Blueprint("sales", __name__)
 collection = db["orders_real_users"]
 
 
-def to_dict(item):
-    item["_id"] = str(item["_id"])
-    item["user"]["_id"] = str(item["user"]["_id"])
-    for item in item["items"]:
-        item["_id"] = str()
-    return item
+BASE_QUERY = [
+    {
+        "$lookup": {
+            "from": "users",
+            "localField": "id_customer",
+            "foreignField": "_id",
+            "as": "user",
+            "pipeline": [
+                    {"$project": {"name": 1, "cpf": 1}},
+                    {"$set": {"_id": {"$toString": "$_id"}}},
+            ],
+        }
+    },
+    {"$unset": ["id_customer"]},
+    {
+        "$set": {
+            "user": {"$arrayElemAt": ["$user", 0]},
+            "items": {
+                "$map": {
+                    "input": "$items",
+                    "as": "item",
+                    "in": {
+                        "_id": {"$toString": "$$item._id"},
+                        "price": {"$toDouble": "$$item.price"},
+                        "qty": "$$item.qty",
+                    },
+                }
+            },
+            "_id": {"$toString": "$_id"},
+        }
+    },
+]
 
 
 @sales.get("/")
 def get_all_orders():
-    query = collection.aggregate(
-        [
-            {
-                "$lookup": {
-                    "from": "users",
-                    "localField": "id_customer",
-                    "foreignField": "_id",
-                    "as": "user",
-                    "pipeline": [
-                        {"$project": {"name": 1, "cpf": 1}},
-                        {"$set": {"_id": {"$toString": "$_id"}}},
-                    ],
-                }
-            },
-            {"$unset": ["id_customer"]},
-            {
-                "$set": {
-                    "user": {"$arrayElemAt": ["$user", 0]},
-                    "items": {
-                        "$map": {
-                            "input": "$items",
-                            "as": "item",
-                            "in": {
-                                "_id": {"$toString": "$$item._id"},
-                                "price": {"$toDouble": "$$item.price"},
-                                "qty": "$$item.qty",
-                            },
-                        }
-                    },
-                    "_id": {"$toString": "$_id"},
-                }
-            },
-        ]
-    )
+    query = collection.aggregate(BASE_QUERY)
 
     return jsonify(list(query))
+
+
+@sales.get("/filter")
+def filter_sales():
+    body = request.get_json()
+
+    query = collection.aggregate(BASE_QUERY + [
+        {
+            "$match": {
+                "user.name": {"$regex": Regex(body.username, "i")},
+            }
+        }
+    ])
